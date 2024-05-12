1760ce896b941222b0ae62f18bbbbc862205b178
ihsanye <ihsany98@gmail.com>
2024-05-13 00:37:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const checkButton = document.getElementById("check-btn");
+const inputText = document.getElementById("text-input");
+const result = document.getElementById("result");
+
+checkButton.addEventListener("click", () => {
+    const text = inputText.value;
+    if (text === "") {
+        alert("Please input a value");
+    } else {
+        if (checkWord()) {
+            result.textContent = `${text} is a palindrome`;
+        } else {
+            result.textContent = `${text} is not a palindrome`;
+        }
+    }
+})
+
+const checkWord = () => {
+    const regex = /[^\w\s]+/gi;
+    const text = inputText.value.replaceAll(regex, "").replaceAll("_", "");
+    const revText = text.split("").reverse().join("");
+    return text === revText ? true : false;
+}
